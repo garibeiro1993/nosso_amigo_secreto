8ecdd0f030aef6668c15b4cfe46ad58ce991adff
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-05-25 17:52:01 -0300

    create service Raffle + Set devise in rpsec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,10 @@ require File.expand_path('../config/environment', __dir__)
 # Support Dir
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+end
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
