8705efd4018bb4e89f1a7ab0f3ce0895bd91aad3
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-06-03 18:18:41 -0300

    models campaign settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :campaigns, dependent: :destroy #quando um User for apagado todas as campanhas que são relacionadas também são.
 end
